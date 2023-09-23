02132d87906b5aafea9223aa19d0b9f3d355df0a
Raiffer <bernardoraiffer@gmail.com>
2023-09-23 13:01:27 -0300

    Implemento do item C

--- Quest1.c
+++ Quest1.c
@@ -3,6 +3,7 @@
 
 typedef struct conta{
     char*nome;
+    int numero;
     int senha;
     float saldo;
     struct conta * prox;
@@ -12,11 +13,12 @@ typedef struct conta{
 cont * inicio = NULL;
 cont * fim = NULL;
 
-void cadastrarConta(char *nome, int senha, float saldo){
+void cadastrarConta(char *nome, int senha,int numero, float saldo){
 
     cont * novo = malloc(sizeof(cont));
     novo->nome = nome;
     novo->senha = senha;
+    novo->numero = numero;
     novo->saldo = saldo;
     novo->prox = NULL;
     novo->ant = NULL;
@@ -50,25 +52,60 @@ void cadastrarConta(char *nome, int senha, float saldo){
     }
 } 
 
-void imprimir(){
+void removerConta(int numero){
+    cont * aux = inicio;
+    while((aux->numero != numero) && (aux != NULL)){
+        aux = aux->prox;
+    }
+    if (aux == NULL){
+        printf("Conta nao encontrada.\n");
+    }else if (aux == inicio){
+        inicio = aux->prox;
+        inicio->ant = NULL;
+        free(aux);
+    }else if (aux == fim){
+        fim = aux->ant;
+        fim->prox = NULL;
+        free(aux);
+    }else{
+        cont * lixo = aux;
+        aux = lixo->prox;
+        aux->ant = lixo->ant;
+        aux = lixo->ant;
+        aux->prox = lixo->prox;
+        free(lixo);
+    } 
+}
 
-    printf("Imprimindo a Lista...\n");
+void visualizar(){
+    printf("Imprimindo a Lista\n");
+
     cont * aux = inicio;
     int count = 1;
 
     while(aux != NULL){
         printf("Conta %d.\n",count);
         printf("Usuario: %s\n", aux->nome);
-        printf("Saldo: %f\n", aux->saldo);
+        printf("Numero da Conta: %d\n", aux->numero);
+        printf("Saldo: %.2f\n", aux->saldo);
         aux = aux->prox;
         count++;
     }
+    printf("\n");
 
 }
 
 int main(){
     
-    cadastrarConta("Bernardo", 1234, 564.45);
-    imprimir();
+    cadastrarConta("Bernardo", 1234,553030, 564.45);
+    visualizar();
+    cadastrarConta("Cecilia", 1434,53030, 1000.45);
+    visualizar();
+    cadastrarConta("Catarina", 1424,55610, 1230.45);
+    cadastrarConta("Luiz Renato", 1134,53031, 2000.45);
+    visualizar();
+    removerConta(53030);
+    visualizar();
+
     return 0;
 }
