5f97cbc424839e0741e12608ceb0c3ebc69da996
Raiffer <144740036+Raiffer@users.noreply.github.com>
2023-09-26 15:54:19 -0300

    Update Questao2.c

--- Questao2.c
+++ Questao2.c
@@ -1,1 +1,28 @@
-#include
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct musica{
+    char*nome;
+    char*cant;
+    int secs;
+    struct musica * prox;
+    struct musica * ant;
+}mus;
+
+mus * inicio = NULL;
+mus * fim = NULL;
+
+void add_na_playlist(char nome, char cant, int secs){
+    
+    mus * novo = malloc(sizeof(mus));
+    novo->nome = nome;
+    novo->cant = cant;
+    novo->secs = secs;
+    novo->prox = fim;
+    novo->ant = inicio;
+    
+    if (inicio == NULL){
+        inicio = novo;
+        fim = novo;
+    }
+}
