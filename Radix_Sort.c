2ab21f1b14b21ac4a86df35c644f90b7fca07012
Raiffer <bernardoraiffer@gmail.com>
2023-10-03 12:58:09 -0300

    reorganização

--- Radix_Sort.c
+++ Radix_Sort.c
@@ -1,27 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void radix_sort(int *v, int n){
-    int k, div;
-    k = max(v, n);
-    int *temp = malloc(sizeof(int)*n);
-    while (k > 0){ 
-        conting_short(v, n , div, temp);
-        div *= 10;
-        k /= 10;
-    }
-    free(temp);
-}
-
-void conting_short(int *v, int n, int div, int*temp){
-    int l;
-    for (int i = 0; i < 10; i++){
-        for (int s = 0; s < n; i++){
-            
-        }
-    }
-}
-
 int *max(int*v, int n){
     int*aux = v[0];
     for (int i = 0; i < n; i++){
@@ -29,4 +8,47 @@ int *max(int*v, int n){
             aux = v[i];
     }
     return aux;
-} 
+}
+
+void conting_sort(int *v, int n, int div, int*temp){
+    int l;
+
+    for (int i = 0; i < 10; i++){
+        int count = 0;
+        for (int s = 0; s < n; i++){
+            int unid;
+            unid = (v[s]/div)%10;
+            if (unid  == s){
+                temp[count] = v[s];
+                count ++;
+            }
+        }
+    }
+    v = temp;
+}
+
+void radix_sort(int *v, int n){
+    int k, div= 1;
+    k = max(v, n);
+    int *temp = malloc(sizeof(int)*n);
+    while (k > 0){ 
+        conting_sort(v, n , div, temp);
+        div *= 10;
+        k /= 10;
+    }
+    free(temp);
+}
+
+void imprimir(int *v, int n){
+    for (int i = 0;i < n; i++){
+        printf("%d\n", v[i]);
+    }
+}
+
+int main(){
+    int * v[5];
+    for (int i = 0; i < 5; i++){
+        scanf("%d", &v[i]);
+    }
+    imprimir(v,5);
+}
