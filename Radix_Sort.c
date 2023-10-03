987c233e42154f5674169d95e89e5c60403a5ac3
Raiffer <bernardoraiffer@gmail.com>
2023-10-03 18:01:24 -0300

    Update Radix_Sort.c

--- Radix_Sort.c
+++ Radix_Sort.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 // Função Max identifica o maior valor dentro de um vetor
-int max(int*v, int n){
+int max(int *v, int n) {
     int aux = v[0];
-    for (int i = 0; i < n; i++){
+    for (int i = 1; i < n; i++) {
         if (v[i] > aux)
             aux = v[i];
     }
@@ -12,48 +12,62 @@ int max(int*v, int n){
 }
 
 // Função Conting Sort que reorganiza o vetor com base na cada decimal dada pelo parâmetro Div
-void conting_sort(int *v, int n, int div, int *temp){
-    int l;
-    int count = 0;
-    for (int i = 0; i < 10; i++){
-        for (int s = 0; s < n; i++){
-            int unid;
-            unid = (v[s]/div)%10;
-            if (unid  == s){
-                temp[count] = v[s];
-                count ++;
-            }
-        }
+void counting_sort(int *v, int n, int div, int *temp) {
+    int count[10] = {0};
+
+    for (int i = 0; i < n; i++) {
+        int unid = (v[i] / div) % 10;
+        count[unid]++;
     }
-    v = temp;
+
+    for (int i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        int unid = (v[i] / div) % 10;
+        temp[count[unid] - 1] = v[i];
+        count[unid]--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        v[i] = temp[i];
+    }
 }
 
 //Função Principal Radix Sort
-void radix_sort(int *v, int n){
-    int k, div= 1; // K é um contador que conta quantas vezes deverá ser feito o conting sort e Div é a posicao do algarismo que é usado para o conting sort
+void radix_sort(int *v, int n) {
+    int k, div = 1; // K é um contador que conta quantas vezes deverá ser feito o counting sort e Div é a posição do algarismo que é usado para o counting sort
     k = max(v, n); // K é o maior valor do vetor que vai sendo reduzido até ser menor que 1
-    int *temp = malloc(sizeof(int)*n); //vetor temporário que reorganiza os elementos do vetor V para depois aplica-los no vetor V
-    while (k >= 1){ 
-        conting_sort(v, n , div, temp); // Conting sort repete - se enquanto k for inteiro
-        div *= 10; // div é multiplicado por 10 para que o proximo conting sort organize os elementos baseados no algarismo seguinte
-        k /= 10; // k é reduzido por 10 para diminuir em um o numero de casas decimais a serem reorganizadas
+    int *temp = malloc(sizeof(int) * n); // vetor temporário que reorganiza os elementos do vetor V para depois aplicá-los no vetor V
+
+    while (k / div > 0) {
+        counting_sort(v, n, div, temp); // Counting sort repete-se enquanto k/div > 0
+        div *= 10; // div é multiplicado por 10 para que o próximo counting sort organize os elementos baseados no algarismo seguinte
     }
+
     free(temp); // libera o espaço ocupado pelo vetor temporário
 }
 
 // Função que imprime cada elemento do vetor
-void imprimir(int *v, int n){
-    for (int i = 0;i < n; i++){
+void imprimir(int *v, int n) {
+    for (int i = 0; i < n; i++) {
         printf("%d\n", v[i]);
     }
 }
 
-int main(){
-    int tam;
-    scanf("");
+int main() {
     int v[5];
-    for (int i = 0; i < 5; i++){
+    printf("Digite 5 números de 4 dígitos separados por espaços:\n");
+    
+    for (int i = 0; i < 5; i++) {
         scanf("%d", &v[i]);
     }
-    imprimir(v,5);
+
+    radix_sort(v, 5);
+    
+    printf("Vetor ordenado:\n");
+    imprimir(v, 5);
+
+    return 0;
 }
